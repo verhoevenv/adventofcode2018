e71c619203e194e35a8dfbb405e7497aca117288
Vincent Verhoeven <verhoevenv@gmail.com>
2018-12-02 18:18:47 +0100

    Control run-on-input with CLI options

--- run-on-input.ts
+++ run-on-input.ts
@@ -11,12 +11,29 @@ export interface DaySolution {
     part2: ProblemSolution;
 }
 
-async function run(moduleForDay: string, part: 'part1' | 'part2', inputFile: string = 'input.txt') {
+async function run(
+    moduleForDay: string,
+    part: 'part1' | 'part2' | 'both' = 'both',
+    inputFile: string = 'input.txt'
+) {
     const input = fs.readFileSync(inputFile, 'utf8');
-    const loadedModule = await import(moduleForDay);
+    const loadedModule = await import(`./${moduleForDay}`);
     const solutions: DaySolution = loadedModule.solution
-    const output = solutions[part](input);
-    console.log(output);
+    if (part === 'both') {
+        console.log(solutions['part1'](input));
+        console.log(solutions['part2'](input));
+    } else {
+        console.log(solutions[part](input));
+    }
 }
 
-run('./day1', 'part1');
+const args = process.argv.slice(2);
+if (args[0] && args[1]) {
+    run(args[0], <any>args[1]);
+} else if (args[0]) {
+    run(args[0]);
+} else {
+    console.log('usage: npm run grab-star <day> <part>');
+    console.log('  <day> is the modulename (like "day1")');
+    console.log('  <part> is "part1", "part2" or "both" (optional, default="both")');
+}
